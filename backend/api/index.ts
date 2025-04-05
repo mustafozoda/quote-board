0bd6af8dc9e09915600087aad01ce8ab8284c838
mustafozoda <mustafozodashq888@gmail.com>
2025-04-06 00:03:55 +0200

    feat: add serverless-http integration and update Vercel configuration

---
+++ backend/api/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import quoteRoutes from "../src/routes/quote.routes";
+import questionRoutes from "../src/routes/question.routes";
+import serverless from "serverless-http";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+// This is a test endpoint to check if the serverless function is working
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from Vercel Serverless!" });
+});
+
+app.use("/quotes", quoteRoutes);
+app.use("/questions", questionRoutes);
+
+module.exports = serverless(app);
